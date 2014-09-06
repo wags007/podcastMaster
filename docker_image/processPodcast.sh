ba6f96de6f9467ddc56bc460b5391b6c4a7b5fd0
Brian Wagner <wags007@gmail.com>
2014-09-06 19:06:20 -0400

    Trying putting the script in a different directory and running my_init from within it. - JBW

---
+++ docker_image/processPodcast.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This is the wrapper for the Fabric process to get the podcast
+# process it and add it to the repository.
+
+#Need to start ssh
+/sbin/my_init &
+sleep 2
+# Variables
+DIR=$(cd $(dirname $0);pwd)
+LOGDIR=${DIR}/output/logs
+DATE=$(date +%Y%m%d_%H%M)
+OUTPUTLOG=${LOGDIR}/${DATE}.output.log
+
+#Make logs directory
+if [ ! -d ${LOGDIR} ]
+then
+    mkdir -p ${LOGDIR}
+fi
+
+read -p "Enter the part of the URL for the podcast after the v= : " -a URLName
+read -p "Enter the podcast number: " -a podcastNumber
+read -p "Enter the podcast name: " -a podcastName
+
+episodeName=${podcastName}"."${podcastNumber}
+
+echo "Command being run:"|tee ${OUTPUTLOG}
+echo fab -f ${DIR}/fabfile.py convert_podcast:podcastName="${episodeName}",videoName="${URLName}",outputdir="${DIR}/output/${episodeName}"|tee ${OUTPUTLOG}
+fab -f ${DIR}/fabfile.py  convert_podcast:podcastName="${episodeName}",videoName="${URLName}",outputdir="${DIR}/output/${episodeName}"|tee ${OUTPUTLOG}
